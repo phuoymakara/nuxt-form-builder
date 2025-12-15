608ecf7f704e4b5461301d068363ae9562630d51
makara-tsc <makara.phuoy@techostartup.center>
2025-12-15 11:39:13 +0700

    Add OG

--- app.vue
+++ app.vue
@@ -3,3 +3,25 @@
     <NuxtPage />
   </UApp>
 </template>
+<script lang="ts">
+const url = useRequestURL();
+const SITE_NAME = "ESB-FORM";
+const SITE_DESCRIPTION = "Form Builder";
+
+useHead({
+  title: SITE_NAME,
+});
+useSeoMeta({
+  title: SITE_NAME,
+  description: SITE_DESCRIPTION,
+  ogTitle: SITE_NAME,
+  ogDescription: SITE_DESCRIPTION,
+  ogSiteName: SITE_NAME,
+  ogUrl: url.href,
+  twitterCard: "summary_large_image",
+  twitterTitle: SITE_NAME,
+  twitterDescription: SITE_DESCRIPTION,
+  ogImage: `${url.origin}/og.png`,
+  twitterImage: `${url.origin}/og.png`,
+});
+</script>

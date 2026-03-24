7b1fe877bb2937b1d76460ec24f4cb4355229c44
makara-tsc <makara.phuoy@tsc.gov.kh>
2026-03-24 16:56:56 +0700

    fix: client only with map

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // ssr: false,
+  ssr: false,
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxt/ui","@nuxtjs/leaflet"],
